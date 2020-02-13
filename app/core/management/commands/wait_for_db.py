7cf8140db8c2553ffae20321385e8abc4e425ada
Dipesh Bamania <infernaze@gmail.com>
2020-02-13 15:57:22 +0530

    Added create ingredient feature

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -15,6 +15,6 @@ class Command(BaseCommand):
             try:
                 db_conn = connections['default']
             except OperationalError:
-                self.stdout.write('Database unavailable, waitin 1 second')
+                self.stdout.write('Database unavailable, Waiting 1 second')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available'))
